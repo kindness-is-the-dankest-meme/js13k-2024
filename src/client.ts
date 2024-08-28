71ef302dee4f007b4715261b7d23e7968b6ae284
Matt Hayes <matt@mysterycommand.com>
2024-08-27 22:27:09 -0500

    feat: keyboard input

--- src/client.ts
+++ src/client.ts
@@ -5,12 +5,16 @@ declare const c: HTMLElementTagNameMap["canvas"];
 
 const win = window;
 const { devicePixelRatio: dpr, requestAnimationFrame: raf } = win;
+const { PI: π } = Math;
+const ππ = π * 2;
 
 const scale = 1 / dpr;
-let w: number, h: number, hw: number, hh: number;
+let w: number, h: number, hw: number, hh: number, x: number, y: number;
 
 const onResize = () => {
   ({ innerWidth: hw, innerHeight: hh } = win);
+  x ??= hw;
+  y ??= hh;
   w = hw * dpr;
   h = hh * dpr;
 
@@ -21,6 +25,29 @@ const onResize = () => {
 
 on(win, "resize", onResize);
 win.dispatchEvent(new Event("resize"));
+
+const onKeyUp = ({ key }: KeyboardEvent) => {
+  switch (key) {
+    case "ArrowUp": {
+      y -= 20;
+      break;
+    }
+    case "ArrowRight": {
+      x += 20;
+      break;
+    }
+    case "ArrowDown": {
+      y += 20;
+      break;
+    }
+    case "ArrowLeft": {
+      x -= 20;
+      break;
+    }
+  }
+};
+
+on(win, "keyup", onKeyUp);
 
 const ctx = c.getContext("2d")!;
 const draw = () => {
@@ -34,6 +61,11 @@ const draw = () => {
   ctx.textBaseline = "middle";
   ctx.fillStyle = "hsla(50, 10%, 90%, 0.1)";
   ctx.fillText("hello", hw, hh);
+
+  ctx.fillStyle = "hsla(50, 10%, 90%, 0.6)";
+  ctx.beginPath();
+  ctx.ellipse(x, y, 20, 20, 0, 0, ππ);
+  ctx.fill();
 };
 
 raf(draw);
