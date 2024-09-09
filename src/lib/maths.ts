1d55c72a03536af041de6b3ba050e83fb5b03015
Matt Hayes <matt@mysterycommand.com>
2024-09-08 23:40:33 -0500

    feat: recursive object / array / any / number lerp

--- src/lib/maths.ts
+++ src/lib/maths.ts
@@ -1,3 +1,71 @@
-export const { atan2, cos, hypot, PI: π, sign, sin, sqrt } = Math;
-export const ππ = π * 2;
-export const hπ = π / 2;
+import { entries, fromEntries, isArray, stringify } from "./platform.ts";
+
+export const { atan2, cos, hypot, min, PI: π, round, sign, sin, sqrt } = Math;
+export const ππ = π * 2,
+  hπ = π / 2;
+
+type Arr = Array<Val>;
+type Num = number; // this type is `number`
+type Obj = { [key: PropertyKey]: Val };
+type Val =
+  | Arr
+  | bigint
+  | boolean
+  | null
+  | Num
+  | Obj
+  | string
+  | symbol
+  | undefined;
+
+const isArr = (x: unknown): x is Arr => isArray(x);
+const lerpArr = (as: Arr, bs: Arr, t: number): Arr =>
+  as.reduce<Arr>(
+    (acc, a, i) => (canLerp(a, bs[i]) && acc.push(lerp(a, bs[i], t)), acc),
+    []
+  );
+
+const isNum = (x: unknown): x is number => typeof x === "number";
+/**
+ * prefer precise method
+ * @see: https://github.com/mattdesl/lerp/blob/master/index.js
+ * @see: https://en.wikipedia.org/wiki/Linear_interpolation#Programming_language_support
+ */
+const lerpNum = (a: number, b: number, t: number): number =>
+  (1 - t) * a + t * b;
+
+const isObj = (x: unknown): x is Obj =>
+  x != null && !isArr(x) && typeof x === "object";
+const lerpObj = (a: Obj, b: Obj, t: number): Val =>
+  fromEntries(
+    entries(a).reduce<[string, Val][]>(
+      (acc, [k, v]) => (
+        canLerp(v, b[k]) && acc.push([k, lerp(v, b[k], t)]), acc
+      ),
+      []
+    )
+  );
+
+const canLerp = (a: unknown, b: unknown): boolean =>
+  a != null && b != null && typeof a === typeof b;
+export const lerp = <T>(a: T, b: T, t: number): T => {
+  switch (true) {
+    case isArr(a) && isArr(b): {
+      return lerpArr(a, b, t) as T;
+    }
+    case isNum(a) && isNum(b): {
+      return lerpNum(a, b, t) as T;
+    }
+    case isObj(a) && isObj(b): {
+      return lerpObj(a, b, t) as T;
+    }
+    case canLerp(a, b): {
+      return round(1 - t) ? a : b;
+    }
+    default: {
+      throw new Error(
+        `Cannot \`lerp(${stringify(a)}, ${stringify(b)}, ${t})\``
+      );
+    }
+  }
+};
