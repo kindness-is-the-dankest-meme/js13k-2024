546ba4296dec2ea61667ed52cf764d2a7324e7e0
Matt Hayes <matt@mysterycommand.com>
2024-09-12 14:21:19 -0500

    chore: re-export more from Object

--- src/lib/platform.ts
+++ src/lib/platform.ts
@@ -3,7 +3,7 @@ export const w = window,
     devicePixelRatio: dpr,
     requestAnimationFrame: raf,
     structuredClone: dup,
-    Object: { entries, fromEntries, is },
+    Object: { entries, fromEntries, is, keys, values },
     Array: { isArray },
     JSON: { parse, stringify },
   } = w;
